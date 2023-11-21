608277fca938fdb709d49bd73beac914c858e284
Steve Deane Jr <wildmechanic@gmail.com>
2023-11-21 16:20:26 -0700

    updates

--- PLC/CC530_ESTOP.py
+++ PLC/CC530_ESTOP.py
@@ -1,8 +1,12 @@
+from time import sleep
+
 from plyer import notification
 from pylogix import PLC
 
+found_estops = []
 
-def send_notification(title, msg, timeout=10):
+
+def send_notification(title, msg, timeout=5):
     notification.notify(
         title=title,
         message=msg,
@@ -11,13 +15,50 @@ def send_notification(title, msg, timeout=10):
     )
 
 
+nc_estops = [
+    "IS11_ES_PB1",
+    "IS12_ES_PB1",
+    "IS13_ES_PB1",
+    "IS14_ES_PB1",
+    "IS15_ES_PB1",
+    "IS16_ES_PB1",
+    "IS17_ES_PB1",
+    "IS18_ES_PB1",
+    "IS21_ES_PB1",
+    "IS22_ES_PB1",
+    "IS23_ES_PB1",
+    "IS24_ES_PB1",
+    "IS25_ES_PB1",
+    "IS26_ES_PB1",
+    "IS11_ES_PB2",
+    "IS12_ES_PB2",
+    "IS13_ES_PB2",
+    "IS14_ES_PB2",
+    "IS15_ES_PB2",
+    "IS16_ES_PB2",
+    "IS17_ES_PB2",
+    "IS18_ES_PB2",
+    "IS21_ES_PB2",
+    "IS22_ES_PB2",
+    "IS23_ES_PB2",
+    "IS24_ES_PB2",
+    "IS25_ES_PB2",
+    "IS26_ES_PB2",
+    "I_GATE1_OK",
+    "I_GATE2_OK",
+]
+
+
 def get_stats(ip):
     with PLC(ip) as comm:
-        stat = comm.Read("IS21_ES_PB1")
-        if stat.Value is None:
-            stat.Value = "Oops!"
-        send_notification("SmartPac", f"ESTOP condition is {stat.Value}")
-        print(stat.Value)
+        # while True:
+        nc_ret = comm.Read(nc_estops)
+        # send_notification("SmartPac", f"ESTOP condition is {stat.Value}")
+        for tag in nc_ret:
+            if tag.Value == False:
+                print(f"{tag.TagName} is {tag.Value}")
+
+        sleep(0.5)
 
 
 get_stats("10.79.216.223")
